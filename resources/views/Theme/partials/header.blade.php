72c10c56e00acf81110c64c96e45fbeea95391e8
YASSINE-Younes <younesyassinee@gmail.com>
2024-12-10 14:37:46 +0100

    Customizing HEADER TUTO 93

--- resources/views/Theme/partials/header.blade.php
+++ resources/views/Theme/partials/header.blade.php
@@ -3,8 +3,8 @@
           <nav class="navbar navbar-expand-lg navbar-light">
               <div class="container box_1620">
                   <!-- Brand and toggle get grouped for better mobile display -->
-                  <a class="navbar-brand logo_h" href="{{ route('theme.index') }}"><img src="{{ asset('assets') }}/img/logo.png"
-                          alt=""></a>
+                  <a class="navbar-brand logo_h" href="{{ route('theme.index') }}"><img
+                          src="{{ asset('assets') }}/img/logo.png" alt=""></a>
                   <button class="navbar-toggler" type="button" data-toggle="collapse"
                       data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                       aria-label="Toggle navigation">
@@ -18,7 +18,7 @@
                           <li class="nav-item @yield('home-active')"><a class="nav-link"
                                   href="{{ route('theme.index') }}">Home</a>
                           </li>
-                        
+
                           <li class="nav-item submenu dropdown @yield('category-active')">
 
                               <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
@@ -41,14 +41,30 @@
                       <!-- End - Add new blog -->
 
                       <ul class="nav navbar-nav navbar-right navbar-social">
-                          <a href="{{ route('register') }}" class="btn btn-sm btn-warning">Register / Login</a>
-                          <!-- <li class="nav-item submenu dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                  aria-expanded="false">Welcome User</a>
-                <ul class="dropdown-menu">
-                  <li class="nav-item"><a class="nav-link" href="blog-details.html">My Blogs</a></li>
-                </ul>
-              </li> -->
+
+                          @if (!Auth::check())
+                              <a href="{{ route('register') }}" class="btn btn-sm btn-warning">Register / Login</a>
+                          @else
+                              <li class="nav-item submenu dropdown">
+                                   
+                                      <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"
+                                          role="button" aria-haspopup="true" aria-expanded="false"> 
+                                          {{ Auth::user()->name }}</a>
+                                  
+
+                                  <ul class="dropdown-menu">
+                                      <li class="nav-item"><a class="nav-link" href="blog-details.html">Update
+                                              Profile</a>
+                                      </li>
+                                      <li class="nav-item"><a class="nav-link" href="blog-details.html">Logout</a></li>
+                                  </ul>
+                              </li>
+                          @endif
+
+
+
+
+
                       </ul>
                   </div>
               </div>

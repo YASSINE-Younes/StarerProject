79c18fc30c9a0574937b263fbacaa3576de2f3d4
YASSINE-Younes <younesyassinee@gmail.com>
2024-12-10 14:24:52 +0100

    USER Login TUTO 92

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,7 +17,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.login');
+        return view('Theme.login');
     }
 
     /**
@@ -29,8 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
-    }
+        return  to_route('theme.index');    }
 
     /**
      * Destroy an authenticated session.
--- resources/views/Theme/login.blade.php
+++ resources/views/Theme/login.blade.php
@@ -1,38 +1,58 @@
-@extends('Theme.master') 
+@extends('Theme.master')
 
- 
-@section('title','Login');
 
- 
-@section('t2','Login');
- 
+@section('title', 'Login');
+
+
+@section('t2', 'Login');
+
 
 @section('content')
-   <!-- ================ contact section start ================= -->
-   <section class="section-margin--small section-margin">
-    <div class="container">
-      <div class="row">
-        <div class="col-6 mx-auto">
-          <form action="#/" class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-            <div class="form-group">
-              <input class="form-control border" name="email" id="email" type="email" placeholder="Enter email address">
+    <!-- ================ contact section start ================= -->
+    <section class="section-margin--small section-margin">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 mx-auto">
+
+                    <form method="POST" action="{{ route('login') }}"class="form-contact contact_form" id="contactForm"
+                        novalidate="novalidate">
+                        @csrf
+
+
+
+                        {{-- =========== CHAMP EMAIL ============== --}}
+                        <div class="form-group">
+
+                            {{-- CHAMP EMAIL  --}}
+                            <input name="email"  value="{{ old('email') }}" class="form-control border"  
+                                type="email" placeholder="Enter email address">
+                            {{-- MESSAGE ERROR EMAIL  --}}
+                            @error('email')
+                                <span class="text-danger"> {{ $message }} </span>
+                            @enderror
+                        </div>
+
+                        {{-- =========== PASSWORD ============== --}}
+                        <div class="form-group">
+                            {{-- CHAMP EMAIL  --}}
+                            <input class="form-control border" name="password" id="name" type="password"
+                                placeholder="Enter your password">
+                            {{-- MESSAGE ERROR EMAIL  --}}
+                            @error('password')
+                                <span class="text-danger"> {{ $message }} </span>
+                            @enderror
+                        </div>
+
+
+                        <div class="form-group text-center text-md-right mt-3">
+                            <a href="{{ route('register') }}" class="mx-3"> Sign Up ? </a>
+                            <button type="submit" class="button button--active button-contactForm">Login</button>
+                        </div>
+                    </form>
+
+                </div>
             </div>
-            <div class="form-group">
-              <input class="form-control border" name="password" id="name" type="password" placeholder="Enter your password">
-            </div>
-            <div class="form-group text-center text-md-right mt-3">
-              <a href="{{ route('register') }}" class="mx-3"> Sign Up  ?     </a>
-              <button type="submit" class="button button--active button-contactForm">Login</button>
-            </div>
-          </form>
         </div>
-      </div>
-    </div>
-  </section>
-	<!-- ================ contact section end ================= -->
+    </section>
+    <!-- ================ contact section end ================= -->
 @endsection
-
-
- 
- 
- 
